a5c46f6c8edd785b9df458bb952ae6ff08068500
I Nyoman Surya Pradipta <suryapradipta8@gmail.com>
2024-11-28 20:53:27 +0800

    isadmin

--- server/src/routes/notification.route.js
+++ server/src/routes/notification.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const notificationController = require('../controllers/notification.controller');
 const auth = require('../middleware/auth');
+const isAdmin = require("../middleware/admin");
 
 // Route to fetch notifications
 router.get('/', auth, notificationController.getNotifications);
 
 // Broadcast announcement
-router.post('/broadcast', auth,notificationController.broadcastAnnouncement);
+router.post('/broadcast', auth, isAdmin, notificationController.broadcastAnnouncement);
 
 // Mark notification as read
 router.patch('/:notificationId/read', auth, notificationController.markNotificationAsRead);
